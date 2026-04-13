1e8f093db2728839bd94123fa1013952b1d0a3d4
teklaos <bogdan.tyta@gmail.com>
2026-04-13 15:44:48 +0200

    feat: initial commit

---
+++ relentless-backend/src/main/java/com/project/relentless/RelentlessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.relentless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelentlessBackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(RelentlessBackendApplication.class, args);
+    }
+
+}
---
+++ relentless-backend/src/test/java/com/project/relentless/RelentlessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.relentless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelentlessBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
