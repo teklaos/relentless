2b4ba572b709276a571b88826644a6aa71e20ce8
teklaos <bogdan.tyta@gmail.com>
2026-04-13 18:00:04 +0200

    chore: add Spotless plugin, include devtools dependency

--- relentless-backend/src/main/java/com/project/relentless/RelentlessBackendApplication.java
+++ relentless-backend/src/main/java/com/project/relentless/RelentlessBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RelentlessBackendApplication {
 
-    static void main(String[] args) {
-        SpringApplication.run(RelentlessBackendApplication.class, args);
-    }
-
+  static void main(String[] args) {
+    SpringApplication.run(RelentlessBackendApplication.class, args);
+  }
 }
--- relentless-backend/src/test/java/com/project/relentless/RelentlessBackendApplicationTests.java
+++ relentless-backend/src/test/java/com/project/relentless/RelentlessBackendApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RelentlessBackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
