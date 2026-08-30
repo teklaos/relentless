82809888bad98983ccc227fd383f3ad2c796c809
teklaos <bogdan.tyta@gmail.com>
2026-08-31 00:29:55 +0200

    refactor: use RestControllerAdvice instead of ControllerAdvice in GlobalExceptionHandler

--- relentless-backend/src/main/java/com/project/relentless/GlobalExceptionHandler.java
+++ relentless-backend/src/main/java/com/project/relentless/GlobalExceptionHandler.java
@@ -25,8 +25,8 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
@@ -34,7 +34,7 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @NullMarked
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   public record ErrorResponse(
